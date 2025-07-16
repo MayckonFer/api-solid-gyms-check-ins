83af456ae4291c37fc660e624f6a95883815a2cb
MayckonFer <mayckonfernandes1@gmail.com>
2025-07-16 10:02:27 -0300

    api-solid

---
+++ src/use-cases/register.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UsersRepository } from "@/repositories/users-repository";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async index({ email, name, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new Error("E-mail already exists.");
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+  }
+}
